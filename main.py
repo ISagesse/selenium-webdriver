45c23731caae427e7f8b054af159f097f654107d
Israel Sagesse <israelsagesse@gmail.com>
2021-07-21 11:08:56 -0400

    making a dictionary of python events

--- main.py
+++ main.py
@@ -17,7 +17,18 @@ driver.get("https://www.python.org/")
 # documentation_link = driver.find_element_by_css_selector(".documentation-widget a")
 # print(documentation_link.text)
 
-bug_link = driver.find_element_by_xpath('//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
-print(bug_link.text)
+# bug_link = driver.find_element_by_xpath('//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+# print(bug_link.text)
+
+event_times = driver.find_elements_by_css_selector(".event-widget time")
+event_names = driver.find_elements_by_css_selector(".event-widget li a")
+events = {}
+for n in range(len(event_times)):
+    events[n] = {
+        'time': event_times[n].text,
+        'name': event_names[n].text
+    }
+
+print(events)
 
 driver.quit()
